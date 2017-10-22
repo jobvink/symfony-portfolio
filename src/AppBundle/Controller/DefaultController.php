aad45a06aba75d55bda3dc4ed485092c1a4ff751
Job Vink <jmcvink@gmail.com>
2017-10-22 18:54:37 +0200

    static portfolio working

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,13 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getRepository('AppBundle:Competence');
+        $competences = $em->findAll();
+
         // replace this example code with whatever you need
         return $this->render(':default:portfolio.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'competences' => $competences
         ]);
     }
 }
